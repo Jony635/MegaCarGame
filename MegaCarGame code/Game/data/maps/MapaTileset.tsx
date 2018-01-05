dd5f98dc84a9f2b4104b97c31697ffc9f23ce932
Óscar Hernández <oscarhernandezgomez@gmail.com>
2018-01-05 19:05:35 +0100

    New map 3

--- MegaCarGame code/Game/data/maps/MapaTileset.tsx
+++ MegaCarGame code/Game/data/maps/MapaTileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="MapaTileset" tilewidth="32" tileheight="32" tilecount="168" columns="14">
- <image source="MapaTileset.png" width="456" height="391"/>
+<tileset name="Tileset" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="Tileset.png" width="96" height="96"/>
 </tileset>
